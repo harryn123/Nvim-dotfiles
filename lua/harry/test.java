36bce542a442c246670ac4d1982be6644da03373
harryn123 <harry.nagi88@gmail.com>
2022-03-19 17:39:33 -0400

    removed test files

--- lua/harry/test.java
+++
@@ -1,9 +0,0 @@
-public class test {
-    public static void main(String args[]) {
-        int x = 0;
-        x = 2;
-        int w = 3;
-        w = 3923;
-
-    }
-}
--- lua/harry/test2.cs
+++
@@ -1,9 +0,0 @@
-class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-        }
-    }
- 
